c7800365cf4387d4d133d42968f87f473fd88a2b
gdbaker <gdbaker@ualberta.ca>
2018-01-18 13:43:50 -0700

    Added base files

---
+++ PlainSight/Source/PlainSight/PlainSightGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PlainSightGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLAINSIGHT_API APlainSightGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
